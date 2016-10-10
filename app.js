54ee547a242f583a73f4998127e62e67d58f0700
Christopher Myers <myers.chris.dev@gmail.com>
2016-10-10 14:45:35 -0600

    names

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ angular.module('scoreApp', ['ui.router'])
       })
       .state('boardGames', {
         url: '/boardGames',
-        templateUrl: '/.boardGames/board-games-tmpl.html',
+        templateUrl: './boardGames/board-games-tmpl.html',
         controller:'boardGamesCtrl'
       })
       .state('sports', {
