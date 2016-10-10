21a78d0edf9feae8c4d21eabf4c34158c52528c7
Christopher Myers <myers.chris.dev@gmail.com>
2016-10-10 14:44:55 -0600

    fixed name

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ angular.module('scoreApp', ['ui.router'])
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: '/home/home-tmpl.html',
+        templateUrl: './home/home-tmpl.html',
         controller:'homeCtrl'
       })
       .state('boardGames', {
         url: '/boardGames',
-        templateUrl: '/boardGames/board-games-tmpl.html',
+        templateUrl: '/.boardGames/board-games-tmpl.html',
         controller:'boardGamesCtrl'
       })
       .state('sports', {
         url: '/sports',
-        templateUrl: '/sports/sports-tmpl.html',
+        templateUrl: './sports/sports-tmpl.html',
         controller:'sportsCtrl'
       })
 
